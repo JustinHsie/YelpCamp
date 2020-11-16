33e844c9da41882f367227937301da9f72125631
Justin Hsie <justinhsie1001@gmail.com>
2020-11-15 17:20:44 -0800

    Added ejs file and set up get route

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+app.get('/', (req, res) => {
+    res.render('home')
 })
-const express = require('express');
-const app = express();
-
-k
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
-const express = require('express');
-const app = express();
-
-
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
-const express = require('express');
-const app = express();
-
-
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
-const express = require('express');
-const app = express();
-
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
