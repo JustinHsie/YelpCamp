6cbaf06b9c41315bcbf14f9c802dfebab8610ec8
Justin Hsie <justinhsie1001@gmail.com>
2020-11-18 16:07:40 -0800

    Added styles

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDb = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit obcaecati explicabo at dicta quis, eaque cupiditate nesciunt pariatur culpa unde ea itaque eveniet assumenda cumque qui nobis, animi fugiat quam? Eveniet libero rem beatae dolorem esse. Cupiditate recusandae eius nulla repellat totam, at sint provident sed illum veniam animi ab deleniti unde modi cumque in. Et nihil animi eveniet adipisci.',
+            price
         })
         await camp.save();
     }
