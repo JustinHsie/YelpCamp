fd93361eb22e2de7eae5b2f6f46b1b39caf118c9
Justin Hsie <justinhsie1001@gmail.com>
2020-11-25 13:54:34 -0800

    Added login

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
-})
+});
 
 router.post('/register', catchAsync(async (req, res) => {
     try {
@@ -20,5 +21,14 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect('/register');
     }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
