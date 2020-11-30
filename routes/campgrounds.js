69e94442dd49e37c3db2f0d2760640f3c89002bb
Justin Hsie <justinhsie1001@gmail.com>
2020-11-29 20:45:16 -0800

    Added .env file

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const campgrounds = require('../controllers/campgrounds')
-
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware')
-
 const catchAsync = require('../utils/catchAsync')
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(
+    /*.post(
         isLoggedIn, 
         validateCampground, 
         catchAsync(campgrounds.createCampground));
+        */
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.file);
+        res.send('IT WORKED?!')
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
